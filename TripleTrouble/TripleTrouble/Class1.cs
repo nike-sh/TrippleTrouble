1e970f22c466895d95d63d76d725bff2ba44c3a7
nike-sh <nikesh.tailor2@gmail.com>
2017-07-20 11:34:43 +0100

    Add files via upload

---
+++ TripleTrouble/TripleTrouble/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TripleTroubleProgram
+{
+    public class Class1
+    {
+        public static HashSet<int> tripleNumberHashSet = new HashSet<int>();
+        public static HashSet<int> doubleNumberHashSet = new HashSet<int>();
+
+        public static void Main()
+        {
+            var isTrippleTrouble = TripleDouble(1234445, 1364492);
+            if (isTrippleTrouble == 1)
+            {
+                System.Console.WriteLine("Is TT");
+            }
+            else if (isTrippleTrouble == 0)
+            {
+                System.Console.WriteLine("Isnt TT");
+            }
+            else
+            {
+                System.Console.WriteLine("something went wrong");
+            }
+
+            System.Console.ReadLine();
+        }
+
+        public static int TripleDouble(long num1, long num2)
+        {
+            int IsTrippleTrouble;
+
+            CheckForTripple(num1);
+            CheckForDouble(num2);
+
+            var x = doubleNumberHashSet;
+            var y = tripleNumberHashSet;
+
+            tripleNumberHashSet.IntersectWith(doubleNumberHashSet);
+
+
+            if (tripleNumberHashSet.Count == 1)
+            {
+                IsTrippleTrouble = 1;
+            }
+            else
+            {
+                IsTrippleTrouble = 0;
+            }
+            return IsTrippleTrouble;
+
+        }
+
+        public static void CheckForTripple(long num1)
+        {
+
+            string firstNumberString = num1.ToString();
+
+            for (int i = 0; i <= 9; i++)
+            {
+                string trippleNumber = ($"{i}{i}{i}");
+                if (firstNumberString.Contains(trippleNumber)) // if NUMBER contains XXX
+                {
+                    tripleNumberHashSet.Add(i);
+                }
+            }
+        }
+
+        public static void CheckForDouble(long num2)
+        {
+            string seccondNumberString = num2.ToString();
+
+            for (int i = 0; i <= 9; i++)
+            {
+                string doubleNumber = ($"{i}{i}");
+                if (seccondNumberString.Contains(doubleNumber)) // if NUMBER contains XX
+                {
+                    doubleNumberHashSet.Add(i);
+                }
+            }
+        }
+
+    }
+}
---
+++ TripleTrouble/TripleTrouble/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace TripleTroubleProgram
+{
+    public class Program
+    {
+        public static int z;
+        public static int[] arrayOfTrippleNumbers = new int[9];
+        public static int[] arrayOfDoubleNumbers = new int[9];
+        public static int arrayPosition = -1;
+
+        public static void Mai3n()
+        {
+            int isTt = TripleTrouble(1234445, 7654432);
+
+            if (isTt == 1)
+            {
+                Console.WriteLine("Is Triple Trouple");
+            }
+            else
+            {
+                Console.WriteLine("Not Triple Trouble");
+            }
+        }
+
+        public static int TripleTrouble(long num1, long num2)
+        {
+            int IsTrippleTrouble;
+
+            checkForTripple(num1);
+            checkForDouble(num2);
+
+           string tripples = arrayOfTrippleNumbers.ToString();
+           string doubles = arrayOfDoubleNumbers.ToString();
+
+            if (tripples.Contains(doubles))
+            {
+                IsTrippleTrouble = 1;
+            }
+            else
+            {
+                IsTrippleTrouble = 0;
+            }
+            return IsTrippleTrouble;
+
+        }
+
+        public static void checkForTripple(long num1)
+        {
+
+            string firstNumberString = num1.ToString();
+
+            for (int i = 0; i <= 9; i++)
+            {
+                string trippleNumber = ($"{i}{i}{i}");
+                if (firstNumberString.Contains(trippleNumber)) // if NUMBER contains 000
+                {
+                    arrayOfTrippleNumbers[arrayPosition + 1] = i;
+                }
+            }
+        }
+
+        public static void checkForDouble(long num2)
+        {
+            string seccondNumberString = num2.ToString();
+
+            for (int i = 0; i <= 9; i++)
+            {
+                string doubleNumber = ($"{i}{i}");
+                if (seccondNumberString.Contains(doubleNumber)) // if NUMBER contains 00
+                {
+                    arrayOfDoubleNumbers[arrayPosition + 1] = i;
+                }
+            }
+        }
+
+    }
+}
